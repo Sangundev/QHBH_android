9f2709b92ead5d9c07a5c9d84f5de33e71c18942
Sangundev <nguyenngocsang1682@gmail.com>
2024-07-21 10:58:09 +0700

    update create delete product

--- app/src/main/java/com/example/myapplication/Class/Product.kt
+++ app/src/main/java/com/example/myapplication/Class/Product.kt
@@ -1,4 +1,9 @@
 package com.example.myapplication.Class
 
-class Product {
-}
+data class Product(
+    val id: String,
+    val name: String,
+    val description: String,
+    val imageResource: Int,
+    val price: Double
+)
